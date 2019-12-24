564c5a8538217f980e3f705a32604d7711303b3e
ashishmangla0 <ashishmangla1991@gmail.com>
2019-12-24 11:44:46 +0530

    add offline page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const Version = '0.0.2';
 const staticCacheName = `site-static-${Version}`;
-const dynamicCache = 'dynamic-chache';
+const dynamicCacheName = 'dynamic-chache';
 const timeStamp = Date.now();
 const assets = [
     '/',
@@ -8,51 +8,45 @@ const assets = [
     '/assets/css/main.css',
     '/assets/css/bootstrap.min.css',
     '/assets/js/app.js',
-    '/manifest.json',
-    '/assets/icons'
-
+    '/pages/fallback.html'
 ]
 // install event
 self.addEventListener('install', event => {
-    
-   // console.log('service worker installed');
+    // console.log('service worker installed');
     event.waitUntil(
-        caches.open(staticCacheName).then(cache =>{
+        caches.open(staticCacheName).then(cache => {
             console.log('caching shell assets');
             cache.addAll(assets)  //assets with chace
         })
     )
-   
-    
 });
-  
+
 // activate event
 self.addEventListener('activate', event => {
     console.log('service worker activated');
-//delete All old caches
-event.waitUntil(
-    caches.keys().then(keys =>{
+    //delete All old caches
+    event.waitUntil(
+        caches.keys().then(keys => {
             //console.log(keys); //
-                return Promise.all(keys
-                    .filter(key => key !== staticCacheName ).map(key => caches.delete(key))
-                    )
-        }
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
     )
-)
-
 });
-  
-  // fetch event
+
+// fetch event
 self.addEventListener('fetch', event => {
     console.log('fetch event', event);
     event.respondWith(
         caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request).then(fetchRes =>{
-                return caches.open(dynamicCache).then(cache =>{
-                    cache.put(event.request.url,fetchRes.clone());
+            return cacheRes || fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
                     return fetchRes;
                 })
             })
-        })
+        }).catch(() => caches.match('/pages/fallback.html'))
     )
 });
